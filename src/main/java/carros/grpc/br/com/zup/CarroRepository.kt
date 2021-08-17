fbc6852ae29dff6596723a27499f29fd14d5a7d6
CaioNovoaAntunes <caio_novoa_antunes@mail.com>
2021-08-16 23:59:37 -0300

    testes

--- src/main/java/carros/grpc/br/com/zup/CarroRepository.kt
+++ src/main/java/carros/grpc/br/com/zup/CarroRepository.kt
@@ -6,35 +6,5 @@ import io.micronaut.data.jpa.repository.JpaRepository
 @Repository
 interface CarroRepository: JpaRepository<Carro, Long> {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     fun existsByPlaca(placa: String): Boolean
 }
